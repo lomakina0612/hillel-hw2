cc645d8735010c06fb9880492a9bed6144fc58dc
Elena Lomakina <lomakina0612@gmail.com>
2024-08-07 20:20:22 +0200

    add border swapping option

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 
-numbers = [-1, 2, 3, 4, 5, 6, 7, 8, -9]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 lower_bound = 2
-upper_bound = 8
+upper_bound = -6
 
 count = 0
 total_sum = 0
 
 if lower_bound > upper_bound:
-    print ('The lower bound must be less than the upper bound.'
-           f'The interval [{lower_bound}, {upper_bound}] does not exist'
+    print ('The lower bound must be less than the upper bound. '
+           f'The interval [{lower_bound}, {upper_bound}] does not exist.'
     )
 else: 
     for number in numbers:
@@ -18,5 +18,5 @@ else:
     print(
         f'Amount of integers from the list {numbers} '
         f'in the interval [{lower_bound}, {upper_bound}] is {count}, '
-        f'and their sum is {total_sum}'
+        f'and their sum is {total_sum}.'
     )
